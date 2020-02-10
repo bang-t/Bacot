8b610d54a00db097ce5ccc8c71a68c1590cf31b9
bang-t <60775024+bang-t@users.noreply.github.com>
2020-02-10 22:30:07 +0800

    Add files via upload

---
+++ tes1.sh
@@ -0,0 +1,3 @@
+z="
+";Jz='nda ';Az='echo';Gz='a';Bz=' "Na';Dz='nda"';Cz='ma A';Hz='slee';Iz='p 4';Kz='Sala';Fz=' Nam';Ez='read';Lz='h"';
+eval "$Az$Bz$Cz$Dz$z$Ez$Fz$Gz$z$Az$z$Hz$Iz$z$Az$Bz$Cz$Jz$Kz$Lz"
